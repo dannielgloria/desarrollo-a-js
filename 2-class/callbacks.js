3e234eb7cba1cfccab76503578fb610d75ace422
dannielgloria <danniel.gloria@gmail.com>
2025-03-20 19:17:56 -0600

    feat: clase 2 callbacks

--- 2-class/callbacks.js
+++ 2-class/callbacks.js
@@ -1,23 +1,42 @@
-//Callback
-function descargarChats(url, funcionCallback) {
-    console.log("Descargando datos desde: " + url);
-    //! Se simula que se piden datos de WhatsApp, 
-    //! para ejemplos practicos se pondra un time out 
-    //! que simulara que esta tardando en conectarse a el servidor de whatsApp
-    setTimeout(()=>{
-        console.log("Se descargaron los datos");
-        funcionCallback();
-    }, 10000);
 
+function procesarTexto(texto, funcionCallBack) {
+    // Simulamos que procesa el texto de entrada
+    let textoProcesado = texto.toUpperCase()
+
+    //llamada a la funcion callback qe va a hacer otra accion con el texto
+    funcionCallBack(textoProcesado);
+    
 }
 
-
-function procesarChats() {
-    console.log("Procesando los chats")
+function mostrarTexto(procesado){
+    console.log("Texto procesado: ", procesado)
 }
 
-function hackear() {
-    console.log("Hackeando el whats de mi ex por que soy toxique")
+function encriptar(textoEncriptar){
+    console.log("Texto antes de encriptar: ", textoEncriptar)
+    const textoEncriptado = CryptoJS.AES.encrypt(textoEncriptar, '1927gtefdh').toString()
+    console.log("Texto encriptado: ", textoEncriptado)
 }
 
-descargarChats("www.whatsapp.com.mx",procesarChats)
+procesarTexto('Me gusta mucho el queso amarillo',encriptar)
+procesarTexto('Mariguanizen la legaliguana',mostrarTexto)
+
+// Mala practica (Sin CallBack)
+function procesarTextoEncriptando(texto) {
+    // Simulamos que procesa el texto de entrada
+    let textoProcesado = texto.toUpperCase()
+
+    console.log("Texto antes de encriptar: ", textoProcesado)
+    const textoEncriptado = CryptoJS.AES.encrypt(textoProcesado, '1927gtefdh').toString()
+    console.log("Texto encriptado: ", textoEncriptado)
+}
+
+function procesarTextoMostrando(texto) {
+    // Simulamos que procesa el texto de entrada
+    let textoProcesado = texto.toUpperCase()
+
+    console.log("Texto procesado: ", textoProcesado)
+}
+
+procesarTextoMostrando("texto")
+procesarTextoEncriptando("texto")
