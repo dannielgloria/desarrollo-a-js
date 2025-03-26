294b33ddc4edce7944c80d82af59c4ceff97a8ce
dannielgloria <danniel.gloria@gmail.com>
2025-03-25 21:31:13 -0600

    feature: clase 4 - Formulario basico - Formulario avanzado - uso de API

--- 4-class/js/form-1.js
+++ 4-class/js/form-1.js
@@ -1,26 +1,28 @@
-document.getElementById('miFormulario').addEventListener('submit', function(event) {
+document.getElementById('miFormulario').addEventListener('submit', function(event){
+    logicaFormulario(event)
+})
+
+function logicaFormulario(event) {
     event.preventDefault(); // Previene el envio del formulario
 
-    const nombre = document.getElementById('nombre').value;
+    const nombre = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
-    const validacion = validarDatos(email,nombre)
+    const validacion = validarDatos(nombre,email)
     if (validacion === true) {
-        console.log('Bienvenid@ a la pagina: ', nombre, " tu email es :", email);
+        console.log('Bienvenid@ a la pagina: ', nombre, " tu email es :", email)
     } else {
         console.error('Acceso denegado')
     }
-    
 
-    
-})
+}
 
-function validarDatos(email,nombre) {
-    if (nombre===""|| email==="") {
-        alert("Ingresa porfavor datos al formulario")
+function validarDatos(name,email) {
+    if (name===""||email==="") {
+        alert("Ingresa porfavor datos en el formulario")
         return false
     } else {
-        alert("Formulario enviado")
+        alert("Formulario Enviado")
         return true
     }
 }
--- 4-class/js/form-2.js
+++ 4-class/js/form-2.js
@@ -1,26 +1,27 @@
-document.getElementById('pwd').addEventListener('input', function(event) {
-    const valor = event.target.value;
-    //const mensaje = valor.length < 8 ? "La contraseña debe tener almenos 8 caracteres." : "";
-    
-    if (valor.length < 8) {
-        mensaje = "La contraseña debe tener almenos 8 caracteres." 
-    }else{
-        mensaje = ""
+async function getCharacter() {
+    const characterId = document.getElementById('characterId').value;
+
+    if(!characterId) return;
+
+    try {
+        const response = await axios.get(`https://rickandmortyapi.com/api/character/${characterId}`);
+        const character = response.data
+        console.log(character)
+        displayCharacter(character)
+    } catch (error) {
+        console.error('Error al traer personaje: ', error)
+        alert('Hubo un error al traer el personaje, porfavor consulta tu consola para mas INFO')
     }
-    document.getElementById('errorMessage').textContent = mensaje
-})
+}
 
-const form = document.getElementById('myForm');
+function displayCharacter(character) {
+    document.getElementById('characterImage').src = character.image
+    document.getElementById('characterName').textContent = character.name
+    document.getElementById('characterDetails').textContent = `Status: ${character.status}, Genero: ${character.gender}, Especie: ${character.species}`
+    document.getElementById('characterCard').style.display = 'block'
+}
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formData = new FormData(form)
-
-    fetch('/api/subir_archivo',{
-        method: "POST",
-        body: formData
-    })
-    .then(res => res.json())
-    .then(data => console.log('Archivo sibido correctamente', data))
-    .catch(error => console.error('Error al subir el archivo', error))
-})
+function refreshPage() {
+    document.getElementById('characterCard').style.display = 'none'
+    document.getElementById('characterId').value = ''
+}
